039740eecaab4042020f95486afc3032f3e06792
Anna <azavas@yandex.ru>
2022-11-24 12:08:46 +0300

    reshenie zadachi

---
+++ zadacha/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] FirstArray = new string[6] { "hello", "2", "world", ":)", "555", "zadanie" };
+string[] SecondArray = new string[FirstArray.Length];
+void FillArray(string[] FirstArray, string[] SecondArray)
+{
+    int count = 0;
+    for (int i = 0; i < FirstArray.Length; i++)
+    {
+        if (FirstArray[i].Length <= 3)
+        {
+            SecondArray[count] = FirstArray[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FillArray(FirstArray, SecondArray);
+PrintArray(SecondArray);
